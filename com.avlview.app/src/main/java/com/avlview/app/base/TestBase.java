850419ee1a04bcb104d101c92c5766577e119197
Murali Krishnan <mkrishnan@mapview.in>
2018-11-26 14:35:04 +0530

    added extend report with add client testcases

--- com.avlview.app/src/main/java/com/avlview/app/base/TestBase.java
+++ com.avlview.app/src/main/java/com/avlview/app/base/TestBase.java
@@ -1,22 +1,35 @@
 package com.avlview.app.base;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 
 import com.avlview.app.utilities.ExcelReader;
 import com.avlview.app.utilities.TestUtil;
 import com.avlview.app.utilities.WebEventListener;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
 
 public class TestBase {
 
 	public static WebDriver driver;
+
+	public ExtentReports extent;
+	public ExtentTest extentTest;
 
 	public static Properties OR = new Properties();
 	public static Properties prop = new Properties();
@@ -66,6 +79,34 @@ public class TestBase {
 		driver.manage().timeouts().implicitlyWait(TestUtil.Imp_wait, TimeUnit.SECONDS);
 		driver.get(prop.getProperty("url"));
 
+	}
+
+	@BeforeTest
+	public void setExtent() {
+		extent = new ExtentReports(System.getProperty("user.dir") + "/test-output/ExtentReport.html", true);
+		extent.addSystemInfo("Host Name", "Murali");
+		extent.addSystemInfo("User Name", "mkrishnan");
+		extent.addSystemInfo("Environment", "QA");
+
+	}
+
+	@AfterTest
+	public void endReport() {
+		extent.flush();
+		extent.close();
+	}
+
+	public static String getScreenshot(WebDriver driver, String screenshotName) throws IOException {
+		String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		// after execution, you could see a folder "FailedTestsScreenshots"
+		// under src folder
+		String destination = System.getProperty("user.dir") + "/FailedTestsScreenshots/" + screenshotName + dateName
+				+ ".png";
+		File finalDestination = new File(destination);
+		FileUtils.copyFile(source, finalDestination);
+		return destination;
 	}
 
 }
--- com.avlview.app/src/test/java/com/avlview/app/testcases/AddClientPageTest.java
+++ com.avlview.app/src/test/java/com/avlview/app/testcases/AddClientPageTest.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.Hashtable;
 
 import org.testng.Assert;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
@@ -15,6 +16,9 @@ import com.avlview.app.pages.AddClientPage;
 import com.avlview.app.pages.ClientsPage;
 import com.avlview.app.pages.LoginPage;
 import com.avlview.app.utilities.TestUtil;
+import com.relevantcodes.extentreports.LogStatus;
+
+import rough.NewTest1;
 
 public class AddClientPageTest extends TestBase {
 
@@ -38,6 +42,9 @@ public class AddClientPageTest extends TestBase {
 
 	@Test(priority = 1, enabled = true)
 	public void TemplateTest() throws IOException, InterruptedException {
+
+		extentTest = extent.startTest("TemplateTest");
+
 		String validateSearchResult = ac.validatesClientsearchresult();
 		String noclienttemplatetxt = AddClientPage.txt;
 		System.out.println(noclienttemplatetxt);
@@ -47,6 +54,9 @@ public class AddClientPageTest extends TestBase {
 
 	@Test(priority = 2, enabled = true)
 	public void ClickAddClientTest() throws IOException {
+
+		extentTest = extent.startTest("ClickAddClientTest");
+
 		ac = cp.addclientclick();
 		String validateaddclient = ac.validateAddclient();
 		Assert.assertEquals(validateaddclient, "Add Client");
@@ -58,7 +68,7 @@ public class AddClientPageTest extends TestBase {
 	 * ac.addclient(); }
 	 */
 
-	@Test(priority = 3, enabled = true, dataProvider = "getData")
+	@Test(priority = 3, enabled = false, dataProvider = "getData")
 	public void AddclientTest(Hashtable<String, String> data) throws IOException, InterruptedException, AWTException {
 		ac = cp.addclientclick();
 		// System.out.println(data.get("isd").substring(0, 2));
@@ -74,14 +84,14 @@ public class AddClientPageTest extends TestBase {
 
 	}
 
-	@Test(priority = 4, enabled = true)
+	@Test(priority = 4, enabled = false)
 	public void SearchAddedClientTest() throws IOException, InterruptedException, AWTException {
 		// ac=cp.addclientclick();
 		String validateSearchResult = ac.validatesClientsearchresult();
 		Assert.assertEquals(validateSearchResult, "1");
 	}
 
-	@Test(priority = 5, enabled = true)
+	@Test(priority = 5, enabled = false)
 	public void deActivateClientTest() throws IOException, InterruptedException, AWTException {
 		// ac=cp.addclientclick();
 		String validateSearchResult = ac.deActivateClient();
@@ -90,29 +100,32 @@ public class AddClientPageTest extends TestBase {
 
 	@Test(priority = 6, enabled = true)
 	public void deactivateSearchTest() throws InterruptedException {
+
+		extentTest = extent.startTest("deactivateSearchTest");
+
 		String validateSearchResult = ac.deActivateClientSearch();
 		Assert.assertEquals(validateSearchResult, "1");
 	}
 
-	@Test(priority = 7, enabled = true)
+	@Test(priority = 7, enabled = false)
 	public void items25Test() throws InterruptedException {
 		boolean cnt = ac.itemsperpage("25");
 		Assert.assertTrue(cnt);
 	}
 
-	@Test(priority = 8, enabled = true)
+	@Test(priority = 8, enabled = false)
 	public void items50Test() throws InterruptedException {
 		boolean cnt = ac.itemsperpage("50");
 		Assert.assertTrue(cnt);
 	}
 
-	@Test(priority = 9, enabled = true)
+	@Test(priority = 9, enabled = false)
 	public void items100Test() throws InterruptedException {
 		boolean cnt = ac.itemsperpage("100");
 		Assert.assertTrue(cnt);
 	}
 
-	@Test(priority = 10, enabled = true)
+	@Test(priority = 10, enabled = false)
 	public void PaginationTest() throws InterruptedException {
 		String paginationtxt = ac.pagination();
 		Assert.assertEquals(paginationtxt, "101 - 200");
@@ -125,8 +138,26 @@ public class AddClientPageTest extends TestBase {
 	}
 
 	@AfterMethod
-	public void teardown() {
+	public void teardown(ITestResult result) throws IOException {
+
+		if (result.getStatus() == ITestResult.FAILURE) {
+			extentTest.log(LogStatus.FAIL, "Test case failed is" + result.getName());
+			extentTest.log(LogStatus.FAIL, "Test case failed is" + result.getThrowable());
+
+			String screenshotPath = NewTest1.getScreenshot(driver, result.getName());
+			extentTest.log(LogStatus.FAIL, extentTest.addScreenCapture(screenshotPath));
+
+		} else if (result.getStatus() == ITestResult.SKIP) {
+			extentTest.log(LogStatus.SKIP, "Test Case SKIPPED IS " + result.getName());
+		} else if (result.getStatus() == ITestResult.SUCCESS) {
+			extentTest.log(LogStatus.PASS, "Test Case PASSED IS " + result.getName());
+
+		}
+
+		extent.endTest(extentTest);
+
 		driver.quit();
+
 	}
 
 }
