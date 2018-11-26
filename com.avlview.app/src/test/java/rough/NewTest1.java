0afe290135ea9cf4ed14b5438879652022835715
Murali Krishnan <mkrishnan@mapview.in>
2018-11-26 13:14:43 +0530

    Extended report included

--- com.avlview.app/src/test/java/rough/NewTest1.java
+++ com.avlview.app/src/test/java/rough/NewTest1.java
@@ -1,34 +1,113 @@
 package rough;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
 public class NewTest1 {
 
 	public static WebDriver driver;
 
-	// @Test(retryAnalyzer = com.avlview.Analyzer.RetryAnalyzer.class)
-	@Test
-	public void launchBrowser() throws InterruptedException {
+	public ExtentReports extent;
+	public ExtentTest extentTest;
 
-		// System.out.println("launching firefox browser");
+	@BeforeTest
+	public void setExtent() {
+		extent = new ExtentReports(System.getProperty("user.dir") + "/test-output/ExtentReport.html", true);
+		extent.addSystemInfo("Host Name", "Murali");
+		extent.addSystemInfo("User Name", "mkrishnan");
+		extent.addSystemInfo("Environment", "QA");
 
-		// System.setProperty("webdriver.gecko.driver", "C:/2018/geckodriver.exe");
-		// driver = new FirefoxDriver();
+	}
 
+	@AfterTest
+	public void endReport() {
+		extent.flush();
+		extent.close();
+	}
+
+	public static String getScreenshot(WebDriver driver, String screenshotName) throws IOException {
+		String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		// after execution, you could see a folder "FailedTestsScreenshots"
+		// under src folder
+		String destination = System.getProperty("user.dir") + "/FailedTestsScreenshots/" + screenshotName + dateName
+				+ ".png";
+		File finalDestination = new File(destination);
+		FileUtils.copyFile(source, finalDestination);
+		return destination;
+	}
+
+	@BeforeMethod
+	public void setup() {
 		System.setProperty("webdriver.chrome.driver", "C:/2018/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 
 		driver.get("https://app.avlview.com");
+	}
 
-		// Thread.sleep(5000);
+	// @Test(retryAnalyzer = com.avlview.Analyzer.RetryAnalyzer.class)
+	@Test
+	public void tittleTest() throws InterruptedException {
 
-		driver.findElement(By.xpath("//input[@id='blinkLogInText']")).sendKeys("test");
-		// driver.findElement(By.xpath("//div[3]/span[2]/a")).click();
+		extentTest = extent.startTest("launchBrowser");
 
-		// driver.quit();
+		String a = driver.getTitle();
+		System.out.println(a);
+
+		Assert.assertEquals(a, "Version 7.0");
+	}
+
+	// @Test(retryAnalyzer = com.avlview.Analyzer.RetryAnalyzer.class)
+	@Test
+	public void logoTest() throws InterruptedException {
+
+		extentTest = extent.startTest("launchBrowser");
+
+		boolean b = driver.findElement(By.xpath("//img[@id='clientimglogo1']")).isDisplayed();
+		Assert.assertTrue(b);
+	}
+
+	@AfterMethod
+	public void teardown(ITestResult result) throws IOException {
+
+		if (result.getStatus() == ITestResult.FAILURE) {
+			extentTest.log(LogStatus.FAIL, "Test case failed is" + result.getName());
+			extentTest.log(LogStatus.FAIL, "Test case failed is" + result.getThrowable());
+
+			String screenshotPath = NewTest1.getScreenshot(driver, result.getName());
+			extentTest.log(LogStatus.FAIL, extentTest.addScreenCapture(screenshotPath));
+
+		} else if (result.getStatus() == ITestResult.SKIP) {
+			extentTest.log(LogStatus.SKIP, "Test Case SKIPPED IS " + result.getName());
+		} else if (result.getStatus() == ITestResult.SUCCESS) {
+			extentTest.log(LogStatus.PASS, "Test Case PASSED IS " + result.getName());
+
+		}
+
+		extent.endTest(extentTest);
+
+		driver.quit();
 
 	}
 
