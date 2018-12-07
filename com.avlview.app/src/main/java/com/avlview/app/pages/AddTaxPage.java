f642a642841227ae9d5589c1732e8edeb4a9411f
Murali Krishnan <mkrishnan@mapview.in>
2018-12-07 16:27:47 +0530

    added tax page

--- com.avlview.app/src/main/java/com/avlview/app/pages/AddTaxPage.java
+++ com.avlview.app/src/main/java/com/avlview/app/pages/AddTaxPage.java
@@ -2,7 +2,11 @@ package com.avlview.app.pages;
 
 import java.io.IOException;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.avlview.app.base.TestBase;
 
@@ -13,6 +17,19 @@ public class AddTaxPage extends TestBase {
 		// TODO Auto-generated constructor stub
 
 		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(xpath = "//span[contains(text(),'Taxes')]")
+	WebElement taxes;
+
+	public String validateTaxPage() {
+
+		WebDriverWait wait = new WebDriverWait(driver, 60); // wait for 5 seconds
+		wait.until(ExpectedConditions.visibilityOf(taxes));
+		System.out.println(taxes.getText());
+
+		return taxes.getText();
+
 	}
 
 }
--- com.avlview.app/src/test/java/com/avlview/app/testcases/AddTaxTest.java
+++ com.avlview.app/src/test/java/com/avlview/app/testcases/AddTaxTest.java
@@ -2,11 +2,18 @@ package com.avlview.app.testcases;
 
 import java.io.IOException;
 
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 import com.avlview.app.base.TestBase;
 import com.avlview.app.pages.AddTaxPage;
 import com.avlview.app.pages.ClientsPage;
 import com.avlview.app.pages.LoginPage;
 import com.avlview.app.pages.SettingsPage;
+import com.relevantcodes.extentreports.LogStatus;
 
 public class AddTaxTest extends TestBase {
 
@@ -18,6 +25,50 @@ public class AddTaxTest extends TestBase {
 	public AddTaxTest() throws IOException {
 		super();
 		// TODO Auto-generated constructor stub
+	}
+
+	@BeforeMethod
+	public void setup() throws IOException {
+		Initialization();
+		lp = new LoginPage();
+		cp = lp.login(prop.getProperty("uname"), prop.getProperty("pwd"));
+		if (cp == null) {
+			Assert.fail("Login failed");
+		}
+
+		sp = cp.settingsclick();
+		atp = sp.taxClick();
+
+	}
+
+	@Test(priority = 1, enabled = true)
+	public void validateTaxpageExistTest() {
+		extentTest = extent.startTest("validateTaxpageExistTest");
+		String validatetaxspage = atp.validateTaxPage();
+		Assert.assertEquals(validatetaxspage, "Taxes");
+	}
+
+	@AfterMethod
+	public void teardown(ITestResult result) throws IOException {
+
+		if (result.getStatus() == ITestResult.FAILURE) {
+			extentTest.log(LogStatus.FAIL, "Test case failed is" + result.getName());
+			extentTest.log(LogStatus.FAIL, "Test case failed is" + result.getThrowable());
+
+			String screenshotPath = TestBase.getScreenshot(driver, result.getName());
+			extentTest.log(LogStatus.FAIL, extentTest.addScreenCapture(screenshotPath));
+
+		} else if (result.getStatus() == ITestResult.SKIP) {
+			extentTest.log(LogStatus.SKIP, "Test Case SKIPPED IS " + result.getName());
+		} else if (result.getStatus() == ITestResult.SUCCESS) {
+			extentTest.log(LogStatus.PASS, "Test Case PASSED IS " + result.getName());
+
+		}
+
+		extent.endTest(extentTest);
+
+		driver.quit();
+
 	}
 
 }
