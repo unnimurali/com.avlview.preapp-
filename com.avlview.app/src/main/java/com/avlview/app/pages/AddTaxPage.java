aea5e107c6abbe16d8fc8629f9255b04a21dfd06
Sudha <Sudha@LAPTOP-K18TVMQ7>
2018-12-09 16:19:17 +0530

    on 09-12

--- com.avlview.app/src/main/java/com/avlview/app/pages/AddTaxPage.java
+++ com.avlview.app/src/main/java/com/avlview/app/pages/AddTaxPage.java
@@ -21,6 +21,12 @@ public class AddTaxPage extends TestBase {
 
 	@FindBy(xpath = "//span[contains(text(),'Taxes')]")
 	WebElement taxes;
+	
+	@FindBy(xpath="//a[@class='green_link margin_right_17 margin_left_14 margin_top_1 fl']")
+	WebElement addtax;
+	
+	@FindBy(xpath="//h3[contains(text(),'You')]")
+	WebElement addtaxtemplate;
 
 	public String validateTaxPage() {
 
@@ -31,5 +37,27 @@ public class AddTaxPage extends TestBase {
 		return taxes.getText();
 
 	}
+	
+	public String validateAddTax() {
+
+		WebDriverWait wait = new WebDriverWait(driver, 60); // wait for 5 seconds
+		wait.until(ExpectedConditions.visibilityOf(addtax));
+		System.out.println(addtax.getText());
+
+		return addtax.getText();
+
+	}
+	
+	public String validateAddTaxTemplate() {
+
+		WebDriverWait wait = new WebDriverWait(driver, 60); // wait for 5 seconds
+		wait.until(ExpectedConditions.visibilityOf(addtaxtemplate));
+		System.out.println(addtaxtemplate.getText());
+
+		return addtaxtemplate.getText();
+
+	}
+	
+	
 
 }
